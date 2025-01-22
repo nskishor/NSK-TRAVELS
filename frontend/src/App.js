5f1c71f21bd310e01e649c1dbbf490d2d61af226
nskishor <145981233+nskishor@users.noreply.github.com>
2025-01-22 08:36:49 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BookingForm from './BookingForm';
+import Schedule from './Schedule';
+
+function App() {
+  return (
+    <div>
+      <h1>Bus Ticket Booking</h1>
+      <BookingForm />
+      <Schedule />
+    </div>
+  );
+}
+
+export default App;
